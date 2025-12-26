898bd9895fa0bc49a61e5cbf259137a37f3971f0
Ratan Swarnkar <ratanswarnkar7052@gmail.com>
2025-12-26 17:47:00 +0530

    second commit

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -6,7 +6,7 @@
                     <h5 class="text-white mb-4">Address</h5>
                     <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Greator Noida, Uttar Pradesh</p>
                     <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 9555872224</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@spgsteel.com</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@spgsteel.in</p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-square btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
